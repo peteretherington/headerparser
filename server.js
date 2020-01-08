3bf8b816be6ce6cb21f5e53673312fe85d106583
Peter Etherington <pjg.etherington@gmail.com>
2020-01-08 15:14:21 -0500

    create whoAmI function

--- server.js
+++ server.js
@@ -19,10 +19,25 @@ app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/views/index.html');
 });
 
-// API endpoint...
-app.get('/api/headerparser', function(req, res) {
-	res.json({ greeting: 'headerparser' });
-});
+const whoAmI = ({ headers, connection }) => {
+	const ipaddress = headers['x-real-ip'] || connection.remoteAddress;
+	const language = headers['accept-language'];
+	const software = headers['user-agent'];
+
+	return {
+		ipaddress,
+		language,
+		software
+	};
+
+	// Example data
+	// 	ipaddress: '159.20.14.100',
+	// 	language: 'en-US,en;q=0.5',
+	// 	software: 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:50.0) Gecko/20100101 Firefox/50.0'
+};
+
+// Who Am I?
+app.get('/api/whoami', (req, res) => res.json(whoAmI(req)));
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
